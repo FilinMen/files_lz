5175f9b7c22a896832459bf5a198d115c3628c2d
FilinMen <fiilinkadabra@tutamail.com>
2024-11-23 06:33:06 +0300

    0.01

--- lion.py
+++ lion.py
@@ -1,8 +1,7 @@
 import docx 
 doc = docx.Document('lion.docx')
-a = doc.add_paragraph
+lenpar = len(doc.paragraphs)
+print(lenpar)
+par1t = doc.paragraphs[0].runs[0].text
+data =(par1t)
 
-'''
-for par in doc.paragraphs:
-    print(par.text)
-'''
